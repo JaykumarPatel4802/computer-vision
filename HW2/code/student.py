c19c3be579decede0dd8d540a2a8d4123b20e91e
Jaykumar Patel <patel.jay4802@gmail.com>
2024-02-21 00:22:40 -0600

    try to improve matches

--- HW2/code/student.py
+++ HW2/code/student.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage import filters, feature, img_as_int
 from skimage.measure import regionprops
+import cv2
+import scipy as sp
 
 def plot_interest_points(image, x, y):
     '''
@@ -180,23 +182,109 @@ def get_features(image, x, y, feature_width):
     # TODO: Your implementation here! See block comments and the handout pdf for instructions
     print(f"Getting features for {len(x)} interest points.")
 
-    image_blurred = filters.gaussian(image, sigma=1)
-    
-    # STEP 1: Calculate the gradient (partial derivatives on two directions) on all pixels.
-    dx = filters.sobel_v(image_blurred)
-    dy = filters.sobel_h(image_blurred)
 
-    # STEP 2: Decompose the gradient vectors to magnitude and direction.
-    magnitude = np.sqrt(dx**2 + dy**2)
-    direction = np.arctan2(dy, dx)
-    print("direction shape: ", direction.shape)
+    def getOrientedFilterUsingDistribution(degree, sigma):
+
+        degree = degree + np.pi/2
+
+        # generate the coordinates
+        x,y = np.mgrid[-1:2, -1:2]
+
+        # rotate the coordinates
+        x_rot = x * np.cos(degree) - y * np.sin(degree)
+        y_rot = x * np.sin(degree) + y * np.cos(degree)
+
+        # create the kernel
+        kernel = np.zeros((3, 3))
+
+        # create the multivariate normal distribution
+        mvn = sp.stats.multivariate_normal([0, 0], [[sigma**2, 0], [0, sigma**2]])
+
+        # sample the distribution at each coordinate
+        kernel = -x_rot * mvn.pdf(np.stack([x_rot, y_rot], axis=-1))
+
+        # normalize the kernel
+        kernel /= np.sum(np.abs(kernel))
+
+        return kernel
+
+    def orientedFilterMagnitude(im: np.ndarray, filter_type_dist: bool = True):
+    # return mag: np.ndarray, theta: np.ndarray
+    # orientations would be 0, 45, 90, 135 degrees
+
+        sigma = 1
+        im = cv2.GaussianBlur(im, (5, 5), sigma)
+        # im = cv2.normalize(im, None, 0, 1, cv2.NORM_MINMAX, dtype=cv2.CV_32FC1) // this makes it worse, but for the previous part, this made it better, ASK
+        
+        num_rows = len(im)
+        num_cols = len(im[0])
+
+        orientations = [0, np.pi/6, np.pi/2, 5*np.pi/6]
+        gray_mags = []
+        magnitude_array = []
+        
+        final_mags = np.ones((num_rows, num_cols))
+
+        filter_set = []
+        
+        for orientation in orientations:
+            # a kernel size of 3x3 was assumed
+
+            orientation = -1 * orientation
+            
+            filter_dist = getOrientedFilterUsingDistribution(orientation, sigma=1)
+
+            # apply the filter
+            gray_mags = cv2.filter2D(im, -1, filter_dist)
+            magnitude_array.append(gray_mags)
+
+            filter_set.append(filter)
+
+        magnitude_array = np.array(magnitude_array)
+        max_magnitude_index = np.argmax(magnitude_array, axis=0)
+        final_mags = np.take_along_axis(magnitude_array, max_magnitude_index[None, ...], axis=0)[0]
+        final_thetas = np.array(orientations)[max_magnitude_index]
+
+        return final_mags, final_thetas, filter_set
+
+
+    print(image.dtype)
+
+    # image_blurred = filters.gaussian(image, sigma=1)
+    
+    # # # STEP 1: Calculate the gradient (partial derivatives on two directions) on all pixels.
+    # dx = filters.sobel_v(image_blurred)
+    # dy = filters.sobel_h(image_blurred)
+
+    # # # STEP 2: Decompose the gradient vectors to magnitude and direction.
+    # magnitude = np.sqrt(dx**2 + dy**2)
+    # direction = np.arctan2(dy, dx)
+
+    # magnitude, direction, filter_set = orientedFilterMagnitude(image)
 
     # STEP 3: For each interest point, calculate the local histogram based on related 4x4 grid cells.
     #         Each cell is a square with feature_width / 4 pixels length of side.
     #         For each cell, we assign these gradient vectors corresponding to these pixels to 8 bins
     #         based on the direction (angle) of the gradient vectors. 
 
+    image_blurred = cv2.GaussianBlur(image, (5, 5), 1)
+
+    sobel_x = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+    sobel_y = sobel_x.T
+
+    x_gradient = cv2.filter2D(image_blurred, -1, sobel_x)
+    y_gradient = cv2.filter2D(image_blurred, -1, sobel_y)
+    direction = np.arctan2(y_gradient, x_gradient)
+    magnitude = np.sqrt(x_gradient**2 + y_gradient**2)
+
     features = []
+
+    def normalize_array(arr):
+        min_val = np.min(arr)
+        max_val = np.max(arr)
+        normalized_arr = (arr - min_val) / (max_val - min_val)
+        return normalized_arr
+
 
     # Iterate over keypoints
     for x_coordinate, y_coordinate in zip(x, y):
@@ -206,23 +294,28 @@ def get_features(image, x, y, feature_width):
 
         # Compute histogram of gradients
         histogram16x16 = []
-        for i in range(4):
-            for j in range(4):
+        for i in range(-2,2):
+            for j in range(-2,2):
                 # Compute histogram of the patch
                 histogram4x4 = np.zeros(8)
                 for k in range(4):
                     for l in range(4):
-                        original_x = x_coordinate + i * 4 + k
-                        original_y = y_coordinate + j * 4 + l
+                        original_x = x_coordinate + (i * 4) + k
+                        original_y = y_coordinate + (j * 4) + l
                         
                         # Check if the pixel is out of the image
                         if original_x < 0 or original_x >= image.shape[0] or original_y < 0 or original_y >= image.shape[1]:
                             continue
 
                         histogram4x4[int(direction[original_x][original_y] / (np.pi / 4))] += magnitude[original_x, original_y]
-                histogram4x4 /= np.linalg.norm(histogram4x4)
+                        
+                        # histogram4x4[int(direction[original_x][original_y] / (np.pi / 4))] += 1
+                # histogram4x4 /= np.linalg.norm(histogram4x4)
+                # histogram4x4 /= np.max(histogram4x4)
+                histogram4x4 = normalize_array(histogram4x4)
                 histogram16x16.append(histogram4x4)
         histogram16x16 = np.array(histogram16x16).flatten()
+        histogram16x16 = normalize_array(histogram16x16)
         features.append(histogram16x16)
 
 
@@ -291,13 +384,14 @@ def match_features(im1_features, im2_features):
     D = np.sqrt(A + B)
     # STEP 2: Sort and find closest features for each feature, then performs NNDR test.
 
-    threshold = 10
+    threshold = 0.97
 
     matches = []
     confidences = []
     for i in range(D.shape[0]):
         sorted_indices = np.argsort(D[i])
-        if D[i][sorted_indices[0]] / D[i][sorted_indices[1]] < threshold:
+        ratio = D[i][sorted_indices[0]] / D[i][sorted_indices[1]] if D[i][sorted_indices[1]] != 0 else 0
+        if ratio < threshold:
             matches.append([i, sorted_indices[0]])
             confidences.append(1 - D[i][sorted_indices[0]] / D[i][sorted_indices[1]])
     matches = np.array(matches)
